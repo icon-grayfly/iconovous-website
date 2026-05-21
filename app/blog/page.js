c3c38c7d53fb214c11e829a8bccccac80af5ded3
icon-grayfly <eriadura.oo@gmail.com>
2026-05-21 11:38:22 +0100

    Fixed blog page build error

---
+++ app/blog/page.js
@@ -0,0 +1,17 @@
+"use client";
+
+export default function BlogPage() {
+  return (
+    <main className="min-h-screen bg-slate-950 text-white flex items-center justify-center">
+      <div className="text-center">
+        <h1 className="text-5xl font-extrabold">
+          ICONOVOUS Blog
+        </h1>
+
+        <p className="mt-4 text-slate-400">
+          Blog content coming soon.
+        </p>
+      </div>
+    </main>
+  );
+}
